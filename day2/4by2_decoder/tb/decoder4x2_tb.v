91bf8b20b0e94f1f2a07f685e4c6eee15fdfd845
ebinjoseph357 <ebinjoseph357@gmail.com>
2026-06-09 21:01:04 +0530

    Update decoder4x2_tb.v

--- day2/4by2_decoder/tb/decoder4x2_tb.v
+++ day2/4by2_decoder/tb/decoder4x2_tb.v
@@ -1,1 +1,24 @@
+module decoder2x4_tb;
 
+    reg [1:0] A_tb;
+    wire [3:0] Y_tb;
+
+    decoder2x4 dut(
+        .A(A_tb),
+        .Y(Y_tb)
+    );
+
+    initial
+    begin
+        $monitor("Time=%0t A=%b Y=%b",
+                  $time, A_tb, Y_tb);
+
+        A_tb = 2'b00; #10;
+        A_tb = 2'b01; #10;
+        A_tb = 2'b10; #10;
+        A_tb = 2'b11; #10;
+
+        $finish;
+    end
+
+endmodule
