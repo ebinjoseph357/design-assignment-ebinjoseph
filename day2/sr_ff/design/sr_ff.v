9b0397403d415b998be31c4610c9adff7207425b
ebinjoseph357 <ebinjoseph357@gmail.com>
2026-06-09 21:31:18 +0530

    Update sr_ff.v

--- day2/sr_ff/design/sr_ff.v
+++ day2/sr_ff/design/sr_ff.v
@@ -10,22 +10,22 @@ always @(posedge clk)
 begin
     case ({S,R})
         2'b00: begin
-            Q    <= Q;      // No change
+            Q    <= Q;      
             Qbar <= Qbar;
         end
 
         2'b01: begin
-            Q    <= 1'b0;   // Reset
+            Q    <= 1'b0;   
             Qbar <= 1'b1;
         end
 
         2'b10: begin
-            Q    <= 1'b1;   // Set
+            Q    <= 1'b1; 
             Qbar <= 1'b0;
         end
 
         2'b11: begin
-            Q    <= 1'bx;   // Invalid state
+            Q    <= 1'bx;  
             Qbar <= 1'bx;
         end
     endcase
